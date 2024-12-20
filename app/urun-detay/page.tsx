dfd71c351e4dc3fc27cdaf9cee9592dc43cfe7d4
medinecnr <medinecnr35@hotmail.com>
2024-12-20 12:19:38 +0300

    .

--- app/urun-detay/page.tsx
+++ app/urun-detay/page.tsx
@@ -63,11 +63,12 @@ function Page() {
     };
   
     if (loading) {
-      return 
-      <div className="flex justify-center items-center min-h-screen">
-        <Spinner color="warning" label="" />
-      </div>
-    } 
+      return (
+        <div className="flex justify-center items-center min-h-screen">
+          <Spinner color="warning" label="" />
+        </div>
+      );
+    }
 
   return (
     <section>
@@ -75,11 +76,12 @@ function Page() {
       <Navbar />
       <div className="container mx-auto relative ">
         <div className="my-6">
-          <Breadcrumbs>
+          <Breadcrumbs aria-label="Ürün Kategorileri">
             <BreadcrumbItem>Lastik</BreadcrumbItem>
             <BreadcrumbItem>Otomobil Lastikleri</BreadcrumbItem>
             <BreadcrumbItem href="/suv">SUV 4x4 Lastikleri</BreadcrumbItem>
-            <BreadcrumbItem href="/urun-detay" className='hidden sm:block'>235/65R16C 12PR 121/119N Lassa Wintus 2 M+S</BreadcrumbItem>
+            <BreadcrumbItem href="/urun-detay" className="hidden sm:block">235/65R16C 12PR 121/119N Lassa Wintus 2 M+S
+            </BreadcrumbItem>
           </Breadcrumbs>
         </div>
 
@@ -98,6 +100,7 @@ function Page() {
                 <button
                   onClick={handlePrevious}
                   className="absolute left-2 top-1/2 transform -translate-y-1/2 p-2 rounded-full"
+                  aria-label="Önceki resme git"
                 >
                   <i className="fa-solid fa-circle-chevron-left text-[#FA8728]"></i>
                 </button>
@@ -105,6 +108,7 @@ function Page() {
                 <button
                   onClick={handleNext}
                   className="absolute right-2 top-1/2 transform -translate-y-1/2 p-2 rounded-full"
+                  aria-label="Sonraki resme git"
                 >
                   <i className="fa-solid fa-circle-chevron-right text-[#FA8728]"></i>
                 </button>
@@ -121,7 +125,7 @@ function Page() {
                   >
                     <img
                       src={image}
-                      alt={`Thumbnail ${index + 1}`}
+                      alt={`Thumbnail ${index + 1} - Lastik Görseli`}
                       className="w-20 h-24 object-cover rounded-xl cursor-pointer"
                       onClick={() => handleThumbnailClick(index)}
                     />
@@ -174,19 +178,19 @@ function Page() {
           {/* Sağ Div */}
           <div className="w-full sm:w-1/2 relative mt-6 sm:mt-0">
             <div>
-            <div className="flex flex-wrap justify-start items-center gap-2 sm:flex-nowrap">
-              <div className="flex items-center justify-center bg-[#006969] text-white text-sm p-2 rounded-lg flex-1 text-center whitespace-nowrap">
-                %20 İndirim
-              </div>
+              <div className="flex flex-wrap justify-start items-center gap-2 sm:flex-nowrap">
+                <div className="flex items-center justify-center bg-[#006969] text-white text-sm p-2 rounded-lg flex-1 text-center whitespace-nowrap">
+                  %20 İndirim
+                </div>
 
-              <div className="flex items-center justify-center bg-[#FFB45F] text-white text-sm p-2 rounded-lg flex-1 text-center whitespace-nowrap">
-                <i className="fa-solid fa-truck-fast text-white mr-1"></i>Kargo Bedava
-              </div>
+                <div className="flex items-center justify-center bg-[#FFB45F] text-white text-sm p-2 rounded-lg flex-1 text-center whitespace-nowrap">
+                  <i className="fa-solid fa-truck-fast text-white mr-1"></i> Kargo Bedava
+                </div>
 
-              <div className="flex items-center justify-center bg-[#FA8728] text-white text-sm p-2 rounded-lg flex-1 text-center whitespace-nowrap">
-                <i className="fa-solid fa-screwdriver-wrench text-white mr-1"></i>Montaj Hizmeti
+                <div className="flex items-center justify-center bg-[#FA8728] text-white text-sm p-2 rounded-lg flex-1 text-center whitespace-nowrap">
+                  <i className="fa-solid fa-screwdriver-wrench text-white mr-1"></i> Montaj Hizmeti
+                </div>
               </div>
-            </div>
 
               <div className="mt-4 w-full">
                 <h6 className="text-[#FA8728]">Lassa</h6>
@@ -195,12 +199,14 @@ function Page() {
                   <h5 className="text-xl">1.796,00 TL</h5>
                 </div>
               </div>
+
               <div className="mt-4 flex items-center gap-4">
                 {/* Sayaç */}
                 <div className="flex items-center">
                   <button
                     className="py-2 px-4 border rounded-l-xl"
                     onClick={decrementQuantity}
+                    aria-label="Azalt"
                   >
                     <i className="fa-solid fa-minus"></i>
                   </button>
@@ -208,22 +214,25 @@ function Page() {
                   <button
                     className="py-2 px-4 border rounded-r-xl"
                     onClick={incrementQuantity}
+                    aria-label="Arttır"
                   >
                     <i className="fa-solid fa-plus"></i>
                   </button>
                 </div>
+
                 <div>
-                  <button className="p-2 bg-[#FA8728] text-white rounded-xl px-5 flex-1 text-center whitespace-nowrap">
+                  <button className="p-2 bg-[#FA8728] text-white rounded-xl px-5 flex-1 text-center whitespace-nowrap" aria-label="Sepete ekle">
                     SEPETE EKLE{" "}
                     <i className="fa-solid fa-cart-shopping text-tiny ps-2"></i>
                   </button>
                 </div>
               </div>
+
               <div className="mt-2 flex justify-between items-center">
                 <p className="text-slate-500">
                   Stok: <span className="font-semibold text-black">{stockQuantity} adet</span>
                 </p>
-                <p className="text-[#FA8728]  hidden sm:block">
+                <p className="text-[#FA8728] hidden sm:block">
                   Taksit Seçenekleri
                   <i className="fa-solid fa-chevron-down ms-1 font-mono"></i>
                 </p>
@@ -246,7 +255,7 @@ function Page() {
                       classNames={{
                         content: ["py-2 px-4 w-48", "text-black "],
                       }}
-                      content="Lastik Borsası dfsd dsfsdfds dsfsdfd dsfdssf dfsdsfds dsfdsds sdfdsfdds sdfsdsdfd"
+                      content="Lastik Borsası hakkında daha fazla bilgi"
                       placement="right"
                     >
                       <i className="fa-solid fa-circle-info text-[#FA8728] ms-2"></i>
@@ -254,7 +263,7 @@ function Page() {
                   </div>
                 </div>
 
-                <p className="text-[#FA8728]  hidden sm:block">
+                <p className="text-[#FA8728] hidden sm:block">
                   Tüm Satıcılar
                   <i className="fa-solid fa-chevron-down ms-1 font-mono"></i>
                 </p>
@@ -272,7 +281,7 @@ function Page() {
                     content="Beğendiklerime Ekle"
                     placement="left"
                   >
-                    <button className="border rounded-xl w-10 h-10">
+                    <button className="border rounded-xl w-10 h-10" aria-label="Beğen">
                       <i className="fa-regular fa-heart"></i>
                     </button>
                   </Tooltip>
@@ -282,9 +291,9 @@ function Page() {
                       content: ["py-2 px-4", "text-black "],
                     }}
                     content="Değerlendirme Yap"
-                    placement="top"
+                    placement="bottom"
                   >
-                    <button className="border rounded-xl w-10 h-10">
+                    <button className="border rounded-xl w-10 h-10" aria-label="Değerlendirme yap">
                       <i className="fa-regular fa-comment"></i>
                     </button>
                   </Tooltip>
@@ -296,7 +305,7 @@ function Page() {
                     content="Paylaş"
                     placement="right"
                   >
-                    <button className="border rounded-xl w-10 h-10">
+                    <button className="border rounded-xl w-10 h-10" aria-label="Paylaş">
                       <i className="fa-solid fa-share"></i>
                     </button>
                   </Tooltip>
@@ -320,109 +329,132 @@ function Page() {
           </div>
         </div>
 
-
-
- {/* Ürün Özellikleri, Değerlendirmeler, Taksit Seçenekleri, İptal ve İade Koşulları, Tüm Satıcılar */}
- <div className="py-6 bg-white">
+        {/* Ürün Özellikleri, Değerlendirmeler, Taksit Seçenekleri, İptal ve İade Koşulları, Tüm Satıcılar */}
+        <div className="py-6 bg-white">
           <div className="flex w-full flex-col py-6 bg-white">
-            <Tabs aria-label="Options" variant='underlined'>
+            <Tabs aria-label="Ürün Bilgileri ve İncelemeleri" variant="underlined">
+              
+              {/* Ürün Özellikleri */}
               <Tab key="urunozellikleri" title="Ürün Özellikleri">
                 <Card>
-                  <CardBody className='p-6'>
-                    <div>
-                      <p className='pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat natus animi alias nisi odit debitis, obcaecati esse, praesentium ipsum iusto voluptates rem expedita commodi mollitia reprehenderit sapiente aperiam, exercitationem nam dolorum quis provident magnam! Eius, dolore, nisi fugiat, perferendis doloribus assumenda labore voluptate ipsam illum nostrum dolorum laboriosam incidunt numquam.</p>
-                    </div>
+                  <CardBody className="p-6">
+                    <h2>Ürün Özellikleri</h2>
+                    <p className="pb-4">
+                      Bu lastik, Yaz mevsimi için tasarlanmış olup, mükemmel yol tutuşu sağlar. 16 inç jant çapı ve 55 kesit oranı ile hem estetik hem de performans açısından ideal bir tercihtir.
+                    </p>
                     <div className="overflow-x-auto">
-                      <table className="table-auto w-1/3 border-separate border-spacing-0">
+                      <table className="table-auto w-full border-collapse">
                         <tbody>
                           <tr>
-                            <td className=" py-2">Mevsim</td>
-                            <td className=" py-2 font-semibold ">Yaz</td>
+                            <td className="py-2 ">Mevsim</td>
+                            <td className="py-2 font-semibold">Yaz</td>
+                          </tr>
+                          <tr className="bg-[#ff7e0613]">
+                            <td className="py-2 rounded-l-lg">Jant Çapı</td>
+                            <td className="py-2 font-semibold rounded-r-lg">16</td>
                           </tr>
                           <tr>
-                            <td className=" py-2 ">Jant Çapı</td>
-                            <td className="py-2 font-semibold">16</td>
-                          </tr>
-                          <tr>
-                            <td className="py-2 ">Kesit Oranı</td>
+                            <td className="py-2">Kesit Oranı</td>
                             <td className="py-2 font-semibold">55</td>
                           </tr>
-                          <tr>
-                            <td className="py-2 ">Taban Genişliği</td>
-                            <td className="py-2 font-semibold">205mm</td>
+                          <tr className="bg-[#ff7e0613]">
+                            <td className="py-2 rounded-l-lg">Taban Genişliği</td>
+                            <td className="py-2 font-semibold rounded-r-lg">205mm</td>
                           </tr>
                           <tr>
-                            <td className="py-2 ">Yüz Endeksi</td>
+                            <td className="py-2">Yüz Endeksi</td>
                             <td className="py-2 font-semibold">91</td>
                           </tr>
-                          <tr>
-                            <td className="py-2 ">Üretim Yılı</td>
-                            <td className="py-2 font-semibold">2023</td>
+                          <tr className="bg-[#ff7e0613] rounded-t-lg">
+                            <td className="py-2 rounded-l-lg">Üretim Yılı</td>
+                            <td className="py-2 font-semibold rounded-r-lg">2023</td>
                           </tr>
                         </tbody>
                       </table>
+
+
                     </div>
                   </CardBody>
                 </Card>
               </Tab>
+
+              {/* Değerlendirmeler */}
               <Tab key="degerlendirmeler" title="Değerlendirmeler">
                 <Card>
                   <CardBody>
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur.
+                    <h3>Ürün Değerlendirmeleri</h3>
+                    <p>
+                      "lorum ipsum dolor sit amet consectetur adipisicing elit. Maiores earum, sed cumque quam ex fuga vel officiis repudiandae at necessitatibus."
+                    </p>
+                    <p>Ortalama puan: 4.5/5</p>
                   </CardBody>
                 </Card>
               </Tab>
-              <Tab key="taksit" title="Taksik Seçenekleri">
+
+              {/* Taksit Seçenekleri */}
+              <Tab key="taksit" title="Taksit Seçenekleri">
                 <Card>
                   <CardBody>
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum.
+                    <h3>Taksit Seçenekleri</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores earum, sed cumque quam ex fuga vel officiis repudiandae at necessitatibus.
+                    </p>
                   </CardBody>
                 </Card>
               </Tab>
+
+              {/* İptal ve İade Koşulları */}
               <Tab key="iptal" title="İptal ve İade Koşulları">
                 <Card>
                   <CardBody>
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum.
+                    <h3>İptal ve İade Koşulları</h3>
+                    <p>
+                      Ürün teslim alındıktan sonra 14 gün içinde iade edilebilir. Ürünler orijinal ambalajında olmalı ve kullanılmamış olmalıdır.
+                    </p>
                   </CardBody>
                 </Card>
               </Tab>
+
+              {/* Tüm Satıcılar */}
               <Tab key="satici" title="Tüm Satıcılar">
                 <Card>
                   <CardBody>
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum.
+                    <h3>Satıcılar</h3>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt quia quibusdam ratione iusto. Illum sequi ipsam perferendis error quos? Ab.
+                    </p>
                   </CardBody>
                 </Card>
               </Tab>
+
             </Tabs>
           </div>
         </div>
 
-
-
-        <div className='pt-6 bg-white pb-8'>
-          <div className="flex flex-col justify-center items-center pt-8 ">
+        {/* Benzer Ürünler */}
+        <div className='bg-white pb-8'>
+          <div className="flex flex-col justify-center items-center ">
             <h2 className="text-2xl font-bold text-center relative group">
               Benzer Ürünler
               <div className="border-t-3 rounded-lg border-[#FA8728] absolute bottom-[-18px] left-1/2 transform -translate-x-1/2 transition-all group-hover:scale-x-100 group-hover:w-full w-20 origin-center"></div>
             </h2>
           </div>
+
           <div className="flex overflow-x-auto mt-4 space-x-6 py-6 bg-white">
             {products.map((product) => (
               <Link key={product.id} href={`/product/${product.id}`} passHref>
                 <div className="group relative w-56 h-auto shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 ease-in-out hover:scale-105 border-small">
+                  
                   {/* Ürün Görseli */}
                   <div>
                     <img
                       src={product.imageUrl}
-                      alt={product.title}
-                      className="object-contain w-full h-40"/>
+                      alt={`Benzer Ürün: ${product.title}`}
+                      title={`Benzer Ürün: ${product.title}`}
+                      className="object-contain w-full h-40"
+                    />
                   </div>
-                  
+
                   {/* Ürün Bilgileri */}
                   <div className="flex flex-col justify-between items-center text-center gap-3 p-6">
                     <h3 className="text-[#FA8728] font-semibold">{product.title}</h3>
@@ -436,17 +468,17 @@ function Page() {
                   {(product.discount || product.hasSpecialIcon || product.tamir) && (
                     <div className="absolute top-2 right-2 flex flex-col gap-2">
                       {product.discount && (
-                        <div className="flex items-center justify-center bg-[#006969] w-9 h-9 rounded-lg">
+                        <div className="flex items-center justify-center bg-[#006969] w-9 h-9 rounded-lg" title={`%${product.discount} İndirim`}>
                           <span className="text-white font-bold text-sm">%{product.discount}</span>
                         </div>
                       )}
                       {product.hasSpecialIcon && (
-                        <div className="flex items-center justify-center bg-[#FFB45F] w-9 h-9 rounded-lg">
+                        <div className="flex items-center justify-center bg-[#FFB45F] w-9 h-9 rounded-lg" title="Ücretsiz Kargo">
                           <i className="fa-solid fa-truck-fast text-white text-xl"></i>
                         </div>
                       )}
                       {product.tamir && (
-                        <div className="flex items-center justify-center bg-[#FA8728] w-9 h-9 rounded-lg">
+                        <div className="flex items-center justify-center bg-[#FA8728] w-9 h-9 rounded-lg" title="Ücretsiz Servis">
                           <i className="fa-solid fa-screwdriver-wrench text-white"></i>
                         </div>
                       )}
@@ -465,7 +497,6 @@ function Page() {
           </div>
         </div>
 
-       
       </div>
       
       <Hizmetler />
